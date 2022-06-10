628c82fb4c20f0fb26456f1a4a73a484f9232577
Miroslav <mira2408@mail.ru>
2022-06-10 17:08:12 +0300

    check deploy

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -12,22 +12,6 @@ export class PostgresConfigService implements SequelizeOptionsFactory {
   constructor(private readonly _configService: ConfigService) {}
 
   createSequelizeOptions(): SequelizeModuleOptions {
-    const url = this._configService.get<string>('DATABASE_URL');
-    if (url) {
-      return {
-        dialect: 'postgres',
-        host: 'ec2-44-195-169-163.compute-1.amazonaws.com',
-        port: 5432,
-        username: 'hvvfswbagsoppt',
-        password:
-          '0d663823c625c1c9df305d658e9e966a8756d5386b8cbd448400dd6a4d38c0b8',
-        database: 'derodaitivshkg',
-        synchronize: true,
-        autoLoadModels: true,
-        models: [User, Role, UserRoles],
-      };
-    }
-
     return {
       dialect: 'postgres',
       host: this._configService.get<string>('DATABASE_HOST'),
