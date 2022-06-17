4e1fab3995e6847874a2701497b0f40db2d12f5e
Miroslav <mira2408@mail.ru>
2022-06-17 14:26:49 +0300

    fix dockerfile and fileService

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM node:16.14.0
 RUN mkdir -p /app
+RUN mkdir -p /dist
 WORKDIR /app
 COPY package*.json /app/
 RUN npm install
 COPY . /app/
+COPY ./dist ./dist
 RUN npm run build
 CMD ["node", "dist/main.js"]
--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -10,37 +10,35 @@ import { FileType } from '../enums/file-type';
 
 @Injectable()
 export class FileService {
-    createFile(type: FileType, file, host): string {
-      try {
-        const fileExtension = file.originalname.split('.').pop();
-        const fileName = `${uuid.v4()}.${fileExtension}`;
-        const filePath = path.resolve(__dirname, '..', 'static', type);
+  createFile(type: FileType, file, host): string {
+    try {
+      const fileExtension = file.originalname.split('.').pop();
+      const fileName = `${uuid.v4()}.${fileExtension}`;
+      const filePath = path.resolve(__dirname, '..', 'static', type);
 
-        if (!fs.existsSync(filePath)) {
-          fs.mkdirSync(filePath, { recursive: true })
-        }
-
-        fs.writeFileSync(path.resolve(filePath, fileName), file.buffer);
-
-        return `${host}/${type}/${fileName}`;
-
-      } catch (e) {
-          throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, { recursive: true });
       }
+
+      fs.writeFileSync(path.resolve(filePath, fileName), file.buffer);
+
+      return `${host}/${type}/${fileName}`;
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 
-    removeFile(file: string) {
-        try {
-            const fileExtension = file.split('/');
-            const fileType = fileExtension[1];
-            const fileName = fileExtension[2];
+  removeFile(file: string) {
+    try {
+      const fileExtension = file.split('/');
+      const fileType = fileExtension[1];
+      const fileName = fileExtension[2];
 
-            const filePath = path.resolve(__dirname, '..', 'static', fileType);
+      const filePath = path.resolve(__dirname, '..', 'static', fileType);
 
-            fs.unlinkSync(path.resolve(filePath, fileName));
-
-        } catch (e) {
-            throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+      fs.unlinkSync(path.resolve(filePath, fileName));
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 }
